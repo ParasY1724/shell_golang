c9e6a752e233eb454fe40542a766b913c5dd1f21
ParasY1724 <parasyerunkar174@gmail.com>
2026-01-15 13:20:24 +0530

    backslash for '

--- app/main.go
+++ app/main.go
@@ -99,54 +99,56 @@ func init() {
 func parseInput(line string) []string {
 	var args []string
 	var current strings.Builder
-	inSingleQuote := false
-	inDoubleQuote := false
-	afterBlackSlash := false
+
+	inSingle := false
+	inDouble := false
+	escaped := false
 
 	for i := 0; i < len(line); i++ {
 		ch := line[i]
-		if afterBlackSlash {
-			afterBlackSlash = false
+
+		if escaped {
 			current.WriteByte(ch)
+			escaped = false
 			continue
 		}
-		switch ch {
 
-		case '\\':
-			if inSingleQuote || inDoubleQuote {
-				current.WriteByte('\\')
-				continue
-			}
-		
-			afterBlackSlash = true		
-
-		case '\'':
-			if inDoubleQuote {
+		if ch == '\\' {
+			if inSingle {
 				current.WriteByte(ch)
-			} else {
-				inSingleQuote = !inSingleQuote
-			}
-		
-		case '"' :
-			if inSingleQuote {
-				current.WriteByte(ch)
-			} else {
-				inDoubleQuote = !inDoubleQuote
-			}
-
-		case ' ', '\t':
-			if inSingleQuote || inDoubleQuote {
-				current.WriteByte(ch)
-			} else {
-				if current.Len() > 0 {
-					args = append(args, current.String())
-					current.Reset()
+			} else if inDouble {
+				if i+1 < len(line) {
+					next := line[i+1]
+					if next == '"' || next == '\\' || next == '$' || next == '`' || next == '\n' {
+						escaped = true
+					} else {
+						current.WriteByte(ch)
+					}
 				}
+			} else {
+				escaped = true
 			}
-
-		default:
-			current.WriteByte(ch)
+			continue
 		}
+
+		if ch == '\'' && !inDouble {
+			inSingle = !inSingle
+			continue
+		}
+		if ch == '"' && !inSingle {
+			inDouble = !inDouble
+			continue
+		}
+
+		if (ch == ' ' || ch == '\t') && !inSingle && !inDouble {
+			if current.Len() > 0 {
+				args = append(args, current.String())
+				current.Reset()
+			}
+			continue
+		}
+
+		current.WriteByte(ch)
 	}
 
 	if current.Len() > 0 {
@@ -155,6 +157,7 @@ func parseInput(line string) []string {
 
 	return args
 }
+
 
 
 func main() {
